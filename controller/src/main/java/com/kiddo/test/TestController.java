5a862f6b8553224a47d5e8248af284cd8b897d47
mayunfei <kiddoma17@gmail.com>
2018-09-15 23:12:06 +0800

    init

---
+++ controller/src/main/java/com/kiddo/test/TestController.java
@@ -0,0 +1,29 @@
+package com.kiddo.test;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/info")
+    public String get() {
+        return "Get!";
+    }
+
+    @PostMapping("/info")
+    public String post() {
+        return "Post!";
+    }
+
+    @PutMapping("/info")
+    public String put(){
+        return "Put!";
+    }
+
+    @DeleteMapping("info")
+    public String delete(){
+        return "Delete!";
+    }
+
+}
